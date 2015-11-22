b9cfa3bbe0589dd2843d89ee17a3ce6bdf9ba418
carenahsu <carena_525@yahoo.com.hk>
2015-11-22 20:04:15 +0800

    Add sort comparator to sortModels

--- app/src/main/java/hk/ust/cse/hunkim/questionroom/QuestionListAdapter.java
+++ app/src/main/java/hk/ust/cse/hunkim/questionroom/QuestionListAdapter.java
@@ -205,7 +205,7 @@ public class QuestionListAdapter extends FirebaseListAdapter<Question> {
 
     @Override
     protected void sortModels(List<Question> mModels) {
-        Collections.sort(mModels);
+        Collections.sort(mModels, Question.sortingComparator);
     }
 
     @Override
